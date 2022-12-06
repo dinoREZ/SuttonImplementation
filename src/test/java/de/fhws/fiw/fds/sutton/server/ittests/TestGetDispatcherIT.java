9d424e141903fdf00de1df88d52e6e36f2cd4ff0
Peter Braun <p.braun@computer.org>
2022-12-06 17:47:14 +0100

    Working on way to populate database

--- src/test/java/de/fhws/fiw/fds/sutton/server/ittests/TestGetDispatcherIT.java
+++ src/test/java/de/fhws/fiw/fds/sutton/server/ittests/TestGetDispatcherIT.java
@@ -14,13 +14,41 @@
 
 package de.fhws.fiw.fds.sutton.server.ittests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class TestGetDispatcherIT {
     @Test
     public void get_dispatcher_200() {
         assertTrue(true);
+    }
+
+    @Test
+    public void get_person_collection_empty() throws Exception {
+        final HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("localhost")
+                .port(8080)
+                .addPathSegment("sd")
+                .addPathSegment("api")
+                .addPathSegment("persons")
+                .build();
+
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(httpUrl)
+                .get()
+                .build();
+
+        final Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
     }
 }

1a1fb99142f0463b619cd402411ea5ca6e5652b6
David Ruppert <david.ruppert@student.fhws.de>
2023-12-12 03:50:38 +0100

    Renamed CourseOfStudentById to CourseOfStudentByIdOperation

--- src/main/java/de/fhws/fiw/fds/implementation/server/database/hibernate/operations/coursesOfStudent/CourseOfStudentByIdOperation.java
+++ src/main/java/de/fhws/fiw/fds/implementation/server/database/hibernate/operations/coursesOfStudent/CourseOfStudentByIdOperation.java
@@ -6,8 +6,8 @@ import de.fhws.fiw.fds.implementation.server.database.hibernate.models.StudentDB
 import de.fhws.fiw.fds.sutton.server.database.hibernate.operations.relation.AbstractReadRelationByIdOperation;
 import jakarta.persistence.EntityManagerFactory;
 
-public class CourseOfStudentById extends AbstractReadRelationByIdOperation<StudentDB, CourseDB, StudentCourseDB> {
-    public CourseOfStudentById(EntityManagerFactory emf, long primaryId, long secondaryId) {
+public class CourseOfStudentByIdOperation extends AbstractReadRelationByIdOperation<StudentDB, CourseDB, StudentCourseDB> {
+    public CourseOfStudentByIdOperation(EntityManagerFactory emf, long primaryId, long secondaryId) {
         super(emf, StudentCourseDB.class, primaryId, secondaryId);
     }
 }

fda8879e7da82c81e8d07cb7175994f370340fdb
David Ruppert <david.ruppert@student.fhws.de>
2024-01-09 18:59:54 +0100

    Wrote inmemory implementation for CoursesOfStudentDao which isn't bidirectional to use as reference for SuttonCreator

---
+++ src/main/java/de/fhws/fiw/fds/implementation/server/database/inmemory/CoursesOfStudentDaoImplReferenceImplementation.java
@@ -0,0 +1,27 @@
+package de.fhws.fiw.fds.implementation.server.database.inmemory;
+
+import de.fhws.fiw.fds.implementation.server.api.models.Course;
+import de.fhws.fiw.fds.implementation.server.database.CoursesOfStudentDao;
+import de.fhws.fiw.fds.implementation.server.database.DaoFactory;
+import de.fhws.fiw.fds.sutton.server.database.IDatabaseAccessObject;
+import de.fhws.fiw.fds.sutton.server.database.inmemory.AbstractInMemoryRelationStorage;
+import de.fhws.fiw.fds.sutton.server.database.results.CollectionModelResult;
+import de.fhws.fiw.fds.sutton.server.database.searchParameter.SearchParameter;
+
+public class CoursesOfStudentDaoImplReferenceImplementation extends AbstractInMemoryRelationStorage<Course> implements CoursesOfStudentDao {
+
+    @Override
+    public CollectionModelResult<Course> readByQuery(long primaryId, String name, SearchParameter searchParameter) {
+        return readByPredicate(primaryId, course -> course.getName().contains(name));
+    }
+
+    @Override
+    public CollectionModelResult<Course> readAll(long primaryId, SearchParameter searchParameter) {
+        return readByPredicate(primaryId, course -> true);
+    }
+
+    @Override
+    protected IDatabaseAccessObject<Course> getSecondaryStorage() {
+        return DaoFactory.getInstance().getCourseDao();
+    }
+}
